36d199a049e98043848e07642c29b3eca6871e41
Timothée Lottaz <timozattol@gmail.com>
2014-11-14 01:01:38 +0100

    Create seed class for Categories

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,30 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('CategoriesTableSeeder');
+
+        $this->command->info('Categories table seeded!');
 	}
+
+}
+
+class CategoriesTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('categories')->delete();
+
+        DB::table('categories')->insert([
+        	['name' => 'Aide à domicile'],
+        	['name' => 'Babysitting'],
+        	['name' => 'Cobaye pour expériences'],
+        	['name' => 'Informatique'],
+        	['name' => 'Job de bureau'],
+        	['name' => 'Promotion'],
+        	['name' => 'Restauration - Hôtellerie'],
+        	['name' => 'Soutien scolaire'],
+        	['name' => 'Autre']
+        ]);
+    }
 
 }
