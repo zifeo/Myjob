7473bcb7aba624a0a4a33a7e43a0b5940c6082ad
Teo Stocco <teo.stocco@epfl.ch>
2014-11-20 20:44:28 +0000

    Change Ad Controller

--- app/controllers/AdController.php
+++ app/controllers/AdController.php
@@ -1,13 +1,37 @@
 <?php
 
-class AdController extends Controller {
-	
-	public function create() {
+class AdController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
 		$categories = Category::lists('name', 'category_id');
 		return View::make('addAd')->with('categories', $categories);
 	}
 
-	public function store() {
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
 		$this->beforeFilter('csrf');
 
 		$categories = Category::lists('name', 'category_id');
@@ -44,6 +68,55 @@ class AdController extends Controller {
 		} else {
 			return 'SUCCESS !';
 		}
-
 	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+
 }
