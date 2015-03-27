46fd4881999a148819df64b65cbe1a96c07bf085
Timothée Lottaz <timozattol@gmail.com>
2015-03-27 23:53:31 +0100

    Fix #16

--- app/views/moderation.blade.php
+++ app/views/moderation.blade.php
@@ -13,15 +13,22 @@
                 <div class="panel-body">
                     <div class="col-md-3 col-xs-12 side-infos">
                         <span class="badge pull-right">{{{ $category_names[$ad->category_id] }}}</span>
-                        <span>{{{ $ad->place or 'Non spécifié' }}}</span><br>
+                        <span>{{{ $ad->place or '-' }}}</span><br>
                         <span>{{{ $ad->contact_first_name }}} {{{ $ad->contact_last_name }}}</span><br>
-                        <span>{{{ $ad->contact_phone }}}</span><br>
+                        <span>{{{ $ad->contact_phone or "Pas de téléphone"}}}</span><br>
                         <span>{{{ $ad->contact_email }}}</span><br>
                         <span><em>{{{ $ad->starts_at }}} : {{{ $ad->ends_at or $ad->starts_at }}} 
                             ({{{ isset($ad->ends_at) ? floor((strtotime($ad->ends_at) - strtotime($ad->starts_at))/(60*60*24)) : 0 }}} j)</em></span><br>
+
                     </div>
                     <div class ="col-md-9 col-xs-12">
-                        {{{ $ad->description }}}
+                        <p>{{{ $ad->description }}}</p>
+                        
+                        <ul>
+                            <li>{{{ $ad->skills or "Pas de skills"}}}</li>
+                            <li>{{{ $ad->duration or "Pas de durée"}}}</li>
+                            <li>{{{ $ad->language or "Pas de langue"}}}</li>
+                        </ul>
                     </div>
                 </div>
 
