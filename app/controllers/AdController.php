731da42dbf3ff51f2b79e595f63bf11b59dec417
Timothée Lottaz <timozattol@gmail.com>
2015-03-28 00:44:11 +0100

    Put back  instead of Input::all()

--- app/controllers/AdController.php
+++ app/controllers/AdController.php
@@ -63,7 +63,7 @@ class AdController extends \BaseController {
 			}
 
 			/* Create the ad in the DB */
-			$url = Ad::create(Input::all());
+			$url = Ad::create($data);
 
 			return Redirect::route('ad.show', $url);
 		}
