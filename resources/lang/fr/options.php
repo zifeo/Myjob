94a8f006026b9c0921029ef6c64bdd30a4635c17
Teo Stocco <teo.stocco@epfl.ch>
2015-09-10 18:15:26 +0200

    remove mistake 's' in options fr

--- resources/lang/fr/options.php
+++ resources/lang/fr/options.php
@@ -5,9 +5,9 @@ return [
 	'sections' => [],
 	
 	'labels' => [
-		'notifications_instant'	=> "Notifications instantanées (dès la modération de l'annonce)",
-		'notifications_day'		=> "Notifications journalière (nouvelles annonces du jour)",
-		'notifications_week'	=> "Notifications hebdomadaire (nouvelles annonces de la semaine)",
+		'notifications_instant'	=> "Notification instantanée (dès la modération de l'annonce)",
+		'notifications_day'		=> "Notification journalière (nouvelles annonces du jour)",
+		'notifications_week'	=> "Notification hebdomadaire (nouvelles annonces de la semaine)",
 	],
 	
 	'placeholders' => [],
