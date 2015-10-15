079f34ebdc248db34dacc6f0966ba8fe607abdfa
Teo Stocco <teo.stocco@epfl.ch>
2015-10-15 22:46:46 +0200

    improve menu template by reducing repetitions

--- resources/views/layout/elements/menu.blade.php
+++ resources/views/layout/elements/menu.blade.php
@@ -1,91 +1,60 @@
-@if ($auth)
-    <div class="ui computer tablet only row text inverted menu">
-        {!! item('jobs', $action) !!}
+<div class="ui computer tablet only row text inverted menu">
+    {!! item('home', $action) !!}
+    @if ($auth || $publisher)
         {!! item('myjobs', $action) !!}
-        {!! item('newjob', $action) !!}
-        @if ($admin)
-            {!! item('moderation', $action) !!}
-        @endif
+    @endif
+    {!! item('newjob', $action) !!}
+    @if ($admin)
+        {!! item('moderation', $action) !!}
+    @endif
+    @if ($auth)git
         {!! item('options', $action) !!}
-        {!! item('help', $action) !!}
-        <div class="right menu">
+    @endif
+    {!! item('help', $action) !!}
+    <div class="right menu">
+        @if ($auth || $publisher)
             <a class="item" href="{{ action('PublicController@disconnect') }}">
                 {{ trans('general.nav.disconnect') }} · {{ $user }}
             </a>
-        </div>
-    </div>
-    <div class="ui mobile only row text inverted icon menu">
-        <a class="item mobile-menu-toggle">
-            <i class="sidebar large icon"></i>
-            {{ $title or 'Myjob' }}
-        </a>
-
-        <div class="right menu">
-            {!! item('disconnect', $action) !!}
-        </div>
-    </div>
-    <div class="ui left vertical inverted sidebar labeled icon menu">
-        {!! item('jobs', $action, 'home') !!}
-        {!! item('myjobs', $action, 'browser') !!}
-        {!! item('newjob', $action, 'add circle') !!}
-        @if ($admin)
-            {!! item('moderation', $action, 'unhide') !!}
-        @endif
-        {!! item('options', $action, 'options') !!}
-        {!! item('help', $action, 'comments') !!}
-        {!! item('disconnect', $action, 'sign out') !!}
-        <a class="item mobile-menu-toggle">
-            <i class="chevron circle right icon"></i>
-            {{ trans('general.back') }}
-        </a>
-    </div>
-@else
-    <div class="ui computer tablet only row text inverted menu">
-        {!! item('home', $action) !!}
-        @if ($publisher)
-            {!! item('myjobs', $action) !!}
-        @endif
-        {!! item('newjob', $action) !!}
-        {!! item('help', $action) !!}
-        <div class="right menu">
-            @if ($publisher)
-                <a class="item" href="{{ action('PublicController@disconnect') }}">
-                    {{ trans('general.nav.disconnect') }} · {{ $user }}
-                </a>
-            @else
-                {!! item('connect', $action) !!}
-            @endif
-        </div>
-    </div>
-    <div class="ui mobile only row text inverted icon menu">
-        <a class="item mobile-menu-toggle">
-            <i class="sidebar large icon"></i>
-            {{ $title or 'Myjob' }}
-        </a>
-
-        <div class="right menu">
-            @if ($publisher)
-                {!! item('disconnect', $action) !!}
-            @else
-                {!! item('connect', $action) !!}
-            @endif
-        </div>
-    </div>
-    <div class="ui left vertical inverted sidebar labeled icon menu">
-        {!! item('home', $action, 'home') !!}
-        @if ($publisher)
-            {!! item('myjobs', $action, 'browser') !!}
-        @endif
-        {!! item('newjob', $action, 'add circle') !!}
-        {!! item('help', $action, 'comments') !!}
-        @if ($publisher)
-            {!! item('disconnect', $action, 'sign out') !!}
         @else
-            {!! item('connect', $action, 'sign in') !!}
+            {!! item('connect', $action) !!}
         @endif
-        <a class="item mobile-menu-toggle">
-            <i class="chevron circle right icon"></i>
-            {{ trans('general.back') }}
-        </a>
     </div>
-@endif
+</div>
+<div class="ui mobile only row text inverted icon menu">
+    <a class="item mobile-menu-toggle">
+        <i class="sidebar large icon"></i>
+        {{ $title or 'Myjob' }}
+    </a>
+
+    <div class="right menu">
+        @if ($auth || $publisher)
+            {!! item('disconnect', $action) !!}
+        @else
+            {!! item('connect', $action) !!}
+        @endif
+    </div>
+</div>
+<div class="ui left vertical inverted sidebar labeled icon menu">
+    {!! item('home', $action, 'home') !!}
+    @if ($auth || $publisher)
+        {!! item('myjobs', $action, 'browser') !!}
+    @endif
+    {!! item('newjob', $action, 'add circle') !!}
+    @if ($admin)
+        {!! item('moderation', $action, 'unhide') !!}
+    @endif
+    @if ($auth)
+        {!! item('options', $action, 'options') !!}
+    @endif
+    {!! item('help', $action, 'comments') !!}
+    @if ($auth || $publisher)
+        {!! item('disconnect', $action, 'sign out') !!}
+    @else
+        {!! item('connect', $action, 'sign in') !!}
+    @endif
+    <a class="item mobile-menu-toggle">
+        <i class="chevron circle right icon"></i>
+        {{ trans('general.back') }}
+    </a>
+</div>
