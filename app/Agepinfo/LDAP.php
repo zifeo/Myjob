ea8e40b996a887e5489706971b0e9f9985b115a9
Timothée Lottaz <timozattol@gmail.com>
2015-11-16 21:25:43 +0100

    Refactor student search
    Use of LDAP native search function instead of "crawling" the list of result

--- app/Agepinfo/LDAP.php
+++ app/Agepinfo/LDAP.php
@@ -12,14 +12,13 @@ use Myjob\Models\User;
 //==========================================================================
 
 final class LDAP {
-
 	const URL = 'ldaps://ldap.epfl.ch';
 	const PORT = 636;
 	const BASE = 'o=epfl,c=ch';
 
 	// Retrieve all the students from the LDAP
 	public static function getAllStudents() {
-		// TODO
+		
 	}
 
 	// Get a student from the LDAP, given its sciper
@@ -29,33 +28,26 @@ final class LDAP {
 		// Authentification
 		$bind = ldap_bind($connection);
 
-		$searchResults = ldap_search($connection, self::BASE, 'uniqueIdentifier=' . $sciper);
+		$searchResults = ldap_search($connection, self::BASE, '(&(uniqueIdentifier=' . $sciper . ')(employeeType=Etudiant))');
 
 		$entries = ldap_get_entries($connection, $searchResults);
 
 		ldap_close($connection);
 
-		// For each entry, check if one of the values of employeetype is "Etudiant" (Student)
-		foreach ($entries as $entry) {
-			if (isset($entry['employeetype']) && count($entry['employeetype']) > 0) {
-				foreach ($entry['employeetype'] as $key => $type) {
-					if ($type == 'Etudiant') {
-						if (isset($entry['givenname']) && 
-							isset($entry['sn']) &&
-							isset($entry['mail'])) {
+		// For each entry, return user if the first name, last name, and email are defined
+		foreach ($entries as $entry)
+			if (isset($entry['givenname']) &&
+				isset($entry['sn']) &&
+				isset($entry['mail'])) {
 
-							$user = new User();
+				$user = new User();
 
-							$user->sciper = $sciper;
-							$user->first_name = $entry['givenname'][0];
-							$user->last_name = $entry['sn'][0];
-							$user->email = $entry['mail'][0];
+				$user->sciper = $sciper;
+				$user->first_name = $entry['givenname'][0];
+				$user->last_name = $entry['sn'][0];
+				$user->email = $entry['mail'][0];
 
-							return $user;
-						}
-					}
-				}
-			}
+				return $user;
 		}
 
 		// Not a student
