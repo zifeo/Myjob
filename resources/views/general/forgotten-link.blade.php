e0a7b9c173a2a803976b12f35da2528b1987ea7d
Timothée Lottaz <timozattol@gmail.com>
2016-04-13 20:38:04 +0200

    Remove newlines &  old TODO

--- resources/views/general/forgotten-link.blade.php
+++ resources/views/general/forgotten-link.blade.php
@@ -3,13 +3,8 @@
 @section('content')
     <div class="row">
         <div class="seven wide column centered">
-            <!-- TODO use trans()-->
             <h2>{{ trans('general.titles.forgotten-link') }}</h2>
-            <p>
-                {{ trans('general.texts.forgotten-link') }}
-
-            </p>
-
+            <p>{{ trans('general.texts.forgotten-link') }}</p>
             <form class="ui form">
                 <div class="field">
                     <input type="text" name="email" placeholder="{{ trans('general.placeholders.forgotten-link-mail') }}">
